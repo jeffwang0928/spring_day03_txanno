308f9d6288941955b740b94afbcec6c0a2548635
bjwangjiannan <wangjiannan@jd.com>
2018-01-24 23:18:31 +0800

    Test

--- src/com/jd/jdbcTemplate/JdbcTemplateDemo1.java
+++ src/com/jd/jdbcTemplate/JdbcTemplateDemo1.java
@@ -2,6 +2,7 @@ package com.jd.jdbcTemplate;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -48,6 +49,30 @@ public class JdbcTemplateDemo1 {
 		//调用jdbcTemplate的方法实现，第二个参数是接口RowMapper，需要自己写类实现接口，自己做数据封装
 		User user = jdbcTemplate.queryForObject(sql, new MyRowMapper(), "lucy");
 		System.out.println(user.toString());
+	}
+	
+	
+	
+	
+	
+	@Test
+	public void testList(){
+		//设置数据库信息
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql:///spring_day03");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		
+		//创建jdbcTemplate对象，设置数据源
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		
+		//调用方法得到对象
+		String sql = "SELECT * FROM user;";
+		
+		//调用jdbcTemplate的方法实现，第二个参数是接口RowMapper，需要自己写类实现接口，自己做数据封装
+		List<User> list = jdbcTemplate.query(sql, new MyRowMapper());
+		System.out.println(list.toString());
 	}
 } 
 
